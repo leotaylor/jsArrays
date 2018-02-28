b5fb42c8536ae311b93d824187441dea1389060d
Leo Taylor <leotaylor723@gmail.com>
2018-02-27 21:18:39 -0600

    challenge2

--- array.js
+++ array.js
@@ -19,9 +19,36 @@ var joinArray = reverseArray.join("");
     // Teacher solution
 var test = "cat";
 
-var opp = test.split("").reverse.().join("");
-if (test === op){
+var opp = test.split("").reverse().join("");
+if (test === opp){
     console.log("same?", true);
 } else{ 
     console.log("same?", false);
 }
+
+var animals = ['fish', 'dog', 'cat'];
+var animalzElement = document.getElementById('animalz');
+
+for(var i=0; i<animals.length; i++){
+    console.log("animal:", animals[i]);
+    animalzElement.innerHTML += "<h5>" + animals[i] + "</h5>";
+}
+
+// CHALLENGE 2 ------------
+
+var book = 'catch 22';
+var num = book.split(" ").pop();
+
+console.log(num);
+
+//  TEACHER SOLUTIONS
+var book = "catch 22";
+var bookSplit = book.split("");
+var realNumz = [];
+for(var j=0; j<bookSplit.length; j++){
+    if(bookSplit[j]*1){
+        realNumz.push(bookSplit[j]);
+        
+    }
+}
+console.log("answer", realNumz.join(""))
